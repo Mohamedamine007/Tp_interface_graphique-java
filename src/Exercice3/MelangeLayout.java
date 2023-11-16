986f5f2abcb26c8aaab2c7de56f969cbe29d0a49
Mohamed amine <aminebaoui1717@gmail.com>
2023-11-16 23:27:08 +0100

    Add comments to the exercise 3

--- src/Exercice3/MelangeLayout.java
+++ src/Exercice3/MelangeLayout.java
@@ -3,6 +3,7 @@ package Exercice3;
 import javax.swing.*;
 import java.awt.*;
 
+// Inheriting from the JFrame class, so we can use its properties directly
 public class MelangeLayout extends JFrame {
 
     // Declaring the panels needed
@@ -23,33 +24,50 @@ public class MelangeLayout extends JFrame {
     JTextField prenomTextField = new JTextField();
     JTextField adresseTextField = new JTextField(30);
 
+    // I stop declaring the stuff that i will need so the code don't get much longer
+    // from now if we need an object we'll create it directly
     public MelangeLayout() {
 
+        // setting the essential configuration of the MelangeLayout window(fenetre)
         setTitle("melange de layout");
         setSize(500, 400);
         setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        // Here is the main panel, that will contain all the coming panels
         mainPanel.setLayout(new BorderLayout());
 
+        // The second panel
         secondPanel.setLayout(new BorderLayout());
         secondPanel.add(fifthPanel, BorderLayout.NORTH);
         secondPanel.add(seventhPanel, BorderLayout.SOUTH);
 
+        // The fifth panel will be grid layout with five rows and one column
+        // Because we don't have label beside text field, we've one below the other
         fifthPanel.setLayout(new GridLayout(5,1));
+        // Adding the labels with their text fields
         fifthPanel.add(nomLabel);
         fifthPanel.add(nomTextField);
         fifthPanel.add(prenomLabel);
         fifthPanel.add(prenomTextField);
         fifthPanel.add(adresseLabel);
 
+        // The second panel has only one textField for the address
         secondPanel.add(adresseTextField);
 
+        // The seventh panel is for sexe specification
+        // It's radioButton so the user can choose only one
+        // Obviously a user can't be man and woman at the same time
         seventhPanel.setLayout(new FlowLayout());
         seventhPanel.add(new JLabel("sexe"));
         seventhPanel.add(new JRadioButton("homme"));
         seventhPanel.add(new JRadioButton("femme"));
 
+
+        // The third panel is for different sports
+        // each sport displayed below the other using the grid layout
+        // as before we've one column with 9 rows
+        // They're all CheckBox input type, so the user can choose more than one
         thirdPanel.setLayout(new GridLayout(9, 1));
         thirdPanel.add(new JCheckBox("tennis"));
         thirdPanel.add(new JCheckBox("squash"));
@@ -61,18 +79,22 @@ public class MelangeLayout extends JFrame {
         thirdPanel.add(new JCheckBox("volley"));
         thirdPanel.add(new JCheckBox("pelanque"));
 
+        // Adding buttons
         fourthPanel.setLayout(new FlowLayout());
         fourthPanel.add(new JButton("ok"));
         fourthPanel.add(new JButton("annuler"));
 
 
 
-
+        // Because the mainPanel is the main and the root one
+        // It's like a blueprint of the layout
+        // so we specified the big parts on it
         add(mainPanel);
         mainPanel.add(secondPanel, BorderLayout.CENTER);
         mainPanel.add(thirdPanel, BorderLayout.EAST);
         mainPanel.add(fourthPanel, BorderLayout.SOUTH);
 
+        // setting the visibility on true, so we can observe what we've done
         setVisible(true);
     }
 
